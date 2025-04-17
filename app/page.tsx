fdc25f0dc65e4b2c9842488820a7c658b02aab2e
DD-YY-SS <jukseo@gmail.com>
2025-04-17 13:07:16 +0900

    🎨 백엔드 페이지 스타일 수정 및 홈 페이지 정리

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="min-h-screen p-6 flex flex-col gap-6 items-center justify-center text-center">
-      <h1 className="text-4xl font-bold">👋 동연이의 머릿속 들여다보기기</h1>
+      <h1 className="text-4xl font-bold">👋 동연이의 머릿속 들여다보기</h1>
       <p className="text-gray-400">학습 주제를 선택하세요.</p>
 
       <div className="flex gap-4 mt-4">
